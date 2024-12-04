0547fc67a7c0adb4adac3e488817a04cef6ab45c
dev-hamster <hamsterwave0925@gmail.com>
2024-12-05 00:38:57 +0900

    chore(jina): 컴포넌트 렌더링 테스트용 페이지 생성 #10

--- src/app/providers/RouterProvider.tsx
+++ src/app/providers/RouterProvider.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 
 import Community from 'pages/community/Community';
+import Demo from 'pages/demo';
 import Home from 'pages/home/Home';
 import NotFound from 'pages/notFound/NotFound';
 
@@ -11,6 +12,7 @@ export const AppRouter = () => {
     <Routes>
       <Route path={ROUTES.HOME} element={<Home />} />
       <Route path={ROUTES.COMMUNITY.COMMUNITY} element={<Community />} />
+      <Route path={ROUTES.DEMO} element={<Demo />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
---
+++ src/pages/demo/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from 'shared/ui';
+
+// 컴포넌트 렌더링 테스트를 위한 /demo 페이지
+const Demo = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+export default Demo;
