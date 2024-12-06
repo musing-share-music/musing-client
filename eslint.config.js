0ea7d8720e8a93a009ded01c0a5e73b01ea8de05
dev-hamster <hamsterwave0925@gmail.com>
2024-12-07 02:21:57 +0900

    chore(jina): react reslint 추가 #10

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js';
 import importPlugin from 'eslint-plugin-import';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
 import noRelativeImportPaths from 'eslint-plugin-no-relative-import-paths';
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
@@ -26,6 +27,7 @@ export default tseslint.config(
       },
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
       import: importPlugin,
@@ -51,6 +53,15 @@ export default tseslint.config(
           allowSeparatedGroups: true,
         },
       ],
+
+      // React 플러그인에서 제공하는 기본 규칙
+      'react/jsx-uses-react': 'off', // React 17 이후 JSX에서는 React import가 불필요
+      'react/jsx-uses-vars': 'error', // JSX에서 사용된 변수는 경고 없이 사용하도록
+      'react/react-in-jsx-scope': 'off', // React 17 이후로 React를 JSX 내에서 반드시 import하지 않아도 됨
+      'react/prop-types': 'off', // TypeScript를 사용할 때 prop-types는 필요없음
+      'react/display-name': 'off', // 디스플레이 이름에 대한 경고를 끄기
+      'react/jsx-key': 'error', // JSX에서 배열을 렌더링할 때 key prop을 필수로 추가
+
       // import 순서는 FSD를 따름
       'import/order': [
         'error',
