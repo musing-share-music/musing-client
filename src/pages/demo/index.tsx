eb57fe0400e2c546fe2131332784d39052be8599
dev-hamster <hamsterwave0925@gmail.com>
2024-12-07 19:01:10 +0900

    feat(jina): 메인 레이아웃 컴포넌트 #11

--- src/pages/demo/index.tsx
+++ src/pages/demo/index.tsx
@@ -1,11 +1,28 @@
-import { Header } from 'shared/ui';
+import styled from '@emotion/styled';
+
+import { MainLayout } from 'shared/ui/';
 
 // 컴포넌트 렌더링 테스트를 위한 /demo 페이지
 const Demo = () => {
   return (
-    <>
-      <Header isLoggedIn />
-    </>
+    <MainLayout>
+      <PlaceHolder />
+      <br />
+      <PlaceHolder />
+      <br />
+      <PlaceHolder />
+      <br />
+      <PlaceHolder />
+      <br />
+      <PlaceHolder />
+    </MainLayout>
   );
 };
 export default Demo;
+
+const PlaceHolder = styled.div`
+  width: 100%;
+  height: 50svh;
+  background: ${({ theme }) => theme.colors[600]};
+  border: 3px solid ${({ theme }) => theme.colors[200]};
+`;
---
+++ src/shared/ui/Layout/MainLayout.tsx
@@ -0,0 +1,53 @@
+import styled from '@emotion/styled';
+
+import { Header, NavBar } from 'shared/ui';
+import { HEADER_HEIGHT } from 'shared/ui/Header/constants';
+import { NAV_BAR_WIDTH } from 'shared/ui/NavBar/constants';
+
+export const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <PageLayout>
+      <Header isLoggedIn />
+      <LeftContainer>
+        <NavBar />
+      </LeftContainer>
+      <RightContainer>
+        <ScrollableContainer>
+          <ContentsContainer>{children}</ContentsContainer>
+        </ScrollableContainer>
+      </RightContainer>
+    </PageLayout>
+  );
+};
+
+const PageLayout = styled.div`
+  display: flex;
+  height: 100svh;
+`;
+
+const LeftContainer = styled.div`
+  width: ${NAV_BAR_WIDTH}px;
+  background: ${({ theme }) => theme.colors[900]};
+  display: flex;
+`;
+
+const RightContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1; // 화면의 남은 공간을 채운다.
+  background: ${({ theme }) => theme.colors[900]};
+`;
+
+const ScrollableContainer = styled.div`
+  flex: 1;
+  overflow-y: auto;
+  padding-top: ${HEADER_HEIGHT}px; // Header 높이를 고려한 여백
+  scroll-behavior: smooth; // 스크롤 시 부드러운 애니메이션
+`;
+
+// 컨텐츠 width는 1280px로 고정
+const ContentsContainer = styled.div`
+  margin: 0 auto;
+  min-width: 1280px;
+  width: 1280px;
+`;
