89cdebc90d8dd6c0b66811e323c7bf32fe949152
dev-hamster <hamsterwave0925@gmail.com>
2024-12-13 16:36:08 +0900

    feat(jina): 모달 접근성을 위한 이벤트 추가  #16

--- src/shared/ui/Modal/BaseModal/index.tsx
+++ src/shared/ui/Modal/BaseModal/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
 import { BackDrop, ModalBox, Title } from 'shared/ui/Modal/styled';
@@ -9,12 +10,60 @@ import { CloseButton } from './CloseButton';
  * base modal을 사용해 모달을 확장
  */
 const BaseModal = ({ open, children, onClose }: BaseModalProps) => {
+  const modalRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (!open || !modalRef.current) return;
+
+    const focusableElements = modalRef.current.querySelectorAll<HTMLElement>(
+      'a, button, textarea, input, select, [tabindex]:not([tabindex="-1"])',
+    );
+    const firstElement = focusableElements[0];
+    const lastElement = focusableElements[focusableElements.length - 1];
+
+    firstElement?.focus(); // 모달 열릴 때 첫 번째 요소로 포커스 이동
+
+    const handleKeyDown = (event: KeyboardEvent) => {
+      switch (event.key) {
+        case 'Escape':
+          onClose();
+          break;
+        case 'Tab':
+          if (focusableElements.length === 0) {
+            event.preventDefault();
+            return;
+          }
+          if (event.shiftKey) {
+            // Shift + Tab: 첫 번째 요소에서 마지막 요소로 이동
+            if (document.activeElement === firstElement) {
+              event.preventDefault();
+              lastElement?.focus();
+            }
+          } else {
+            // Tab: 마지막 요소에서 첫 번째 요소로 이동
+            if (document.activeElement === lastElement) {
+              event.preventDefault();
+              firstElement?.focus();
+            }
+          }
+          break;
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [open, onClose]);
+
   if (!open) return;
 
   return createPortal(
     <>
       <BackDrop onClick={onClose} />
-      <ModalBox>{children}</ModalBox>
+      <ModalBox ref={modalRef} role="dialog" aria-modal="true" tabIndex={-1}>
+        {children}
+      </ModalBox>
     </>,
     document.body,
   );
