8a740a27fb6b20213c4d17d64dfa73666c445dda
jsnam <skawotkd12@naver.com>
2025-02-04 15:21:17 +0900

    chore(jsnam) : zustand 로컬스토리지 기능 추가

--- src/shared/store/userInfo.ts
+++ src/shared/store/userInfo.ts
@@ -1,33 +1,12 @@
 import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
 
 import type { UserInfo, UserState } from 'shared/types/userInfo';
 
-export const useUserInfoStore = create<UserState>((set, get) => ({
-  // 로그인한 사용자의 정보
-  userInfo: {
-    email: '',
-    name: '',
-    authority: '',
-    likeMusicCount: 0,
-    myPlaylistCount: 0,
-  },
-  passModal: 'notLogIn', // 기본값 설정
-
-  // 로그인 여부 확인
-  isLogin: () => get().passModal !== 'notLogIn',
-
-  // 관리자 여부 확인
-  isAdmin: () => get().userInfo.authority === 'ADMIN',
-
-  // 사용자 정보 설정
-  setUser: (user: UserInfo) => set({ userInfo: user }),
-
-  // 사용자 모달창 여부 설정
-  setPassModal: (value: string) => set({ passModal: value }),
-
-  // 로그아웃 처리
-  logout: () => {
-    set({
+export const useUserInfoStore = create<UserState>()(
+  persist(
+    (set, get) => ({
+      // 로그인한 사용자의 정보
       userInfo: {
         email: '',
         name: '',
@@ -35,7 +14,47 @@ export const useUserInfoStore = create<UserState>((set, get) => ({
         likeMusicCount: 0,
         myPlaylistCount: 0,
       },
-      passModal: 'notLogIn',
-    });
-  },
-}));
+      passModal: 'notLogIn', // 기본값 설정
+
+      // 로그인 여부 확인
+      isLogin: () => {
+        // localStorage에서 직접 확인
+        const storedState = localStorage.getItem('user-storage');
+        if (storedState) {
+          const { state } = JSON.parse(storedState);
+          return state.passModal !== 'notLogIn';
+        }
+        return get().passModal !== 'notLogIn';
+      },
+
+      // 관리자 여부 확인
+      isAdmin: () => get().userInfo.authority === 'ADMIN',
+
+      // 사용자 정보 설정
+      setUser: (user: UserInfo) => set({ userInfo: user }),
+
+      // 사용자 모달창 여부 설정
+      setPassModal: (value: string) => set({ passModal: value }),
+
+      // 로그아웃 처리
+      logout: () => {
+        set({
+          userInfo: {
+            email: '',
+            name: '',
+            authority: '',
+            likeMusicCount: 0,
+            myPlaylistCount: 0,
+          },
+          passModal: 'notLogIn',
+        });
+        // 로그아웃 시 localStorage 클리어
+        localStorage.removeItem('user-storage');
+      },
+    }),
+    {
+      name: 'user-storage',
+      storage: createJSONStorage(() => localStorage),
+    },
+  ),
+);
