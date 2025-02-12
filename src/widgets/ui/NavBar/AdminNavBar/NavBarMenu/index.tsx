704ef37e0049e836941eddcfb4727b6f1a509c31
dev-hamster <hamsterwave0925@gmail.com>
2025-02-12 16:46:43 +0900

    feat(jina): 관리자 nav item 디자인 적용 #37

--- src/widgets/ui/NavBar/AdminNavBar/NavBarMenu/index.tsx
+++ src/widgets/ui/NavBar/AdminNavBar/NavBarMenu/index.tsx
@@ -1,16 +1,35 @@
+import { useState } from 'react';
+
 import { ADMIN_NAV_ITEM } from 'widgets/config/navItem';
 import { NavBarSizeProps } from 'widgets/ui/NavBar/AdminNavBar/type';
 
-import { NavBarItem } from './NavBarItem';
+import { NavBarItem, SubNavBarItem } from './NavBarItem';
+import { SubNavLinkContainer } from './NavBarItem/styled';
 import { NavBarItemList, NavContainer } from './styled';
 
 export const NavBarMenu = ({ size }: NavBarSizeProps) => {
+  const [openItem, setOpenItem] = useState(false);
+
   return (
     <NavContainer size={size}>
       <NavBarItemList size={size}>
-        {ADMIN_NAV_ITEM.map((item) => (
-          <NavBarItem key={item.text} size={size} {...item} />
-        ))}
+        {ADMIN_NAV_ITEM.map((item) => {
+          if (item.children) {
+            return (
+              <>
+                <NavBarItem size={size} text={item.text} onClick={() => setOpenItem((prev) => !prev)} />
+                {openItem && (
+                  <SubNavLinkContainer>
+                    {item.children.map((i) => (
+                      <SubNavBarItem key={i.text} size={size} text={i.text} href={i.href} />
+                    ))}
+                  </SubNavLinkContainer>
+                )}
+              </>
+            );
+          }
+          return <NavBarItem key={item.text} size={size} text={item.text} href={item.href} />;
+        })}
       </NavBarItemList>
     </NavContainer>
   );
