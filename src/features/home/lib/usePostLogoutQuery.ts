1b1cf51e2f5d39569b1f0bf488ba3787bd85ede0
jsnam <skawotkd12@naver.com>
2025-02-27 14:07:24 +0900

    chore(jsnam) : 로그아웃 시 홈으로 이동 제거

--- src/features/home/lib/usePostLogoutQuery.ts
+++ src/features/home/lib/usePostLogoutQuery.ts
@@ -1,17 +1,14 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { useNavigate } from 'react-router-dom';
 
 import { fetchPostLogout } from 'entities/home/api/LogoutPost';
 
 export const useLogoutPostMutation = () => {
   const queryClient = useQueryClient();
-  const navigate = useNavigate();
 
   const LogoutMutation = useMutation({
     mutationFn: () => fetchPostLogout(),
     onSuccess: async () => {
       await queryClient.invalidateQueries();
-      setTimeout(() => navigate('/'), 0);
     },
   });
   return LogoutMutation;
