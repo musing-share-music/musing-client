81b355907eafe5b80d903a62a76d3f6e5fa318bc
dev-hamster <hamsterwave0925@gmail.com>
2025-05-01 22:20:21 +0900

    refactor(jina): 페이지 전환시 레이아웃을 유지하도록 Outlet로 변경

--- src/pages/memberInfo/ui/community.tsx
+++ src/pages/memberInfo/ui/community.tsx
@@ -1,13 +1,7 @@
-import { MainLayout } from 'widgets/ui/Layout';
-
 import { MemberInfoCommunity } from 'features/memberInfo/ui/community';
 
 const MemberCommunity = () => {
-  return (
-    <MainLayout>
-      <MemberInfoCommunity></MemberInfoCommunity>
-    </MainLayout>
-  );
+  return <MemberInfoCommunity></MemberInfoCommunity>;
 };
 
 export default MemberCommunity;
