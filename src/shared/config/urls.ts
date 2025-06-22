f4fe7efdb931816c01a3dbeb8cf022e74e7b7f9a
JTH-00 <dbzgtsa@gmail.com>
2025-06-23 08:37:44 +0900

    fix(JTH-00) : 삭제된 게시글 조회 관련 api url 수정

--- src/shared/config/urls.ts
+++ src/shared/config/urls.ts
@@ -53,7 +53,7 @@ const URL = {
       PERMIT_BOARD: 'musing/admin/board/permit', // 게시글 승인
       REJECT_BOARD: 'musing/admin/board/non/permit', // 게시글 승인 거절
       REMOVED_BOARD_LIST: 'musing/admin/board/list/removed', // 삭제된 게시글 리스트
-      REMOVED_BOARD_DETAIL: 'musing/admin/board/list/removed/detail', // 삭제된 게시글 상세
+      REMOVED_BOARD_DETAIL: 'musing/admin/board/removed', // 삭제된 게시글 상세
     },
 
     // 플레이리스트
